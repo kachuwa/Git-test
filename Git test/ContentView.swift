d11597cf98a0ba63af43bd674e684ecdd1bd371e
Anish Shrestha <anizz.stha14@gmail.com>
2024-01-14 11:20:49 +0545

    Changed to pink

--- Git test/ContentView.swift
+++ Git test/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 Text("This is the content View")
                 NavigationLink(destination: ModalView()){
                     Text("Go Next")
-                }.foregroundColor(.purple)
+                }.foregroundColor(.pink)
                     .navigationTitle("Xcode and Gitz")
             }
         }
