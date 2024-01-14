90ecfb8db63e161c3afa6a63e78afba56d4a84a7
Anish Shrestha <anizz.stha14@gmail.com>
2024-01-14 07:24:55 +0545

    Created seperate ModalView

---
+++ ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  Git test
+//
+//  Created by Anish Shrestha on 14/01/2024.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalView()
+}
