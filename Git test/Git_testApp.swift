cecb9ef0280f1bdac7daf638034e2821bbb8ba5c
Anish Shrestha <anizz.stha14@gmail.com>
2024-01-14 07:15:16 +0545

    Initial Commit

---
+++ Git test/Git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_testApp.swift
+//  Git test
+//
+//  Created by Anish Shrestha on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
